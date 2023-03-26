72aff3e59becc58e3f74395969c4344eaf56484c
Alex42 <alexcrazy42@mail.ru>
2023-03-26 23:14:41 +0300

    first commit

---
+++ LearningCScharp/OOP/OOP.cs
@@ -0,0 +1,38 @@
+﻿
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        p.Person person = new p.Person("Alex", 12);
+        
+    }
+}
+
+namespace p
+{
+    class Person
+    {
+        private string name = "";
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+
+        public string GetName() => name;
+        public int GetAge() => age;
+
+        public void SetName(string name) => this.name = name;
+
+        public void SetAge(int age) => this.age = age;
+    }
+}
+
+
+
+
+ 
