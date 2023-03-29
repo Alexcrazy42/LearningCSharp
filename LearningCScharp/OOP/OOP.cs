c6ba48657b593b9cec3932b371e6fe885ed9f8ab
Alex42 <alexcrazy42@mail.ru>
2023-03-29 23:05:22 +0300

    29.03.2022 1

--- LearningCScharp/OOP/OOP.cs
+++ LearningCScharp/OOP/OOP.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Parallel.Invoke(
+    Print,
+    () =>
+    {
+        Console.WriteLine($"Выполняется задача {Task.CurrentId}, хехе");
+    },
+    () => Square(5)
+);
+
+void Print()
+{
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+}
+// вычисляем квадрат числа
+void Square(int n)
+{
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}, Результат {n * n}");
+}
