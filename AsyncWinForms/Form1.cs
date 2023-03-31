0244976bb3db2382c5d5ef73fb1bdd4ca64b911c
Alex42 <alexcrazy42@mail.ru>
2023-03-31 22:19:24 +0300

    31.03.2023 2

--- AsyncWinForms/Form1.cs
+++ AsyncWinForms/Form1.cs
@@ -15,6 +15,28 @@ namespace AsyncWinForms
         public Form1()
         {
             InitializeComponent();
+            label2.Text = "1";
+        }
+
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            await Task.Run(async () =>
+            {
+                
+                while(i < 1000)
+                {
+                    await Task.Delay(100);
+                    i++;
+                    label1.Text = i.ToString();
+                }
+            }
+            );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = (Int32.Parse(label2.Text) + 1).ToString();
         }
     }
 }
