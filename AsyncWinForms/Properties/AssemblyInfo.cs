f0f097c1a709adb411823f782d38d92a15bd3359
Alex42 <alexcrazy42@mail.ru>
2023-03-31 20:15:13 +0300

    31.03.2023 добавил проект asyncwinforms

---
+++ AsyncWinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("AsyncWinForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AsyncWinForms")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("21af2ad0-8f82-4cd6-80b8-8823d125c237")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- LearningCScharp/OOP/OOP.cs
+++ LearningCScharp/OOP/OOP.cs
@@ -1,18 +1,1 @@
-﻿Parallel.Invoke(
-    Print,
-    () =>
-    {
-        Console.WriteLine($"Выполняется задача {Task.CurrentId}, хехе");
-    },
-    () => Square(5)
-);
-
-void Print()
-{
-    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
-}
-// вычисляем квадрат числа
-void Square(int n)
-{
-    Console.WriteLine($"Выполняется задача {Task.CurrentId}, Результат {n * n}");
-}
+﻿
