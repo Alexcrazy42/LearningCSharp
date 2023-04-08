fbd38fe4e33bc0a3360e6b7f183a8d1d76de79fe
Alex42 <alexcrazy42@mail.ru>
2023-04-08 23:09:56 +0300

    08.04 add mediatR learn (so much problems):

---
+++ MediatR/Program.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using System.Globalization;
+
+public class SomeEvent : INotification
+{
+
+    public string Message { get; }
+    public SomeEvent(string message)
+    {
+        Message = message;
+    }
+}
+
+public class Handler1 : INotificationHandler<SomeEvent>
+{
+    private readonly ILogger<Handler1> _logger;
+
+    public Handler1(ILogger<Handler1> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Handle(SomeEvent notification)
+    {
+        _logger.LogWarning($"Handled: {notification.Message}");
+    }
+}
+
+public class Handler2 : INotificationHandler<SomeEvent>
+{
+    private readonly ILogger<Handler2> _logger;
+
+    public Handler2(ILogger<Handler2> logger)
+    {
+        _logger = logger;
+    }
+    public void Handle(SomeEvent notification)
+    {
+        _logger.LogWarning($"Handled: {notification.Message}");
+    }
+}
+
+
+public class HomeController : Controller
+{
+
+    private readonly IMediator _mediator;
+
+    public HomeController(IMediator mediator)
+    {
+        this._mediator = mediator;
+    }
+    public async Task<IActionResult> Index()
+    {
+        await _mediator.Publish(new SomeEvent("Hello World"));
+        return View();
+    }
+    // more code omitted
+}
