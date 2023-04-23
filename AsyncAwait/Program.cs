21d41c3a9614caed3f538c06c610fae894d397df
Alex42 <alexcrazy42@mail.ru>
2023-04-23 11:14:30 +0300

    async await update

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,14 +1,43 @@
-﻿Print();
+﻿List<Task> list = new List<Task>();
+list.Add(Print1());
+list.Add(Print2());
+list.Add(Print3());
 
-async void Print()
+
+foreach (Task t in list)
 {
-    Console.WriteLine("start");
+    t.ContinueWith(t => { });
+    t.Wait();
+}
+
+
+async Task Print1()
+{
+    Console.WriteLine("start1");
     await Task.Run(() =>
     {
-        Task.Delay(100);
-        Console.WriteLine("in");
+        Thread.Sleep(2000); // imitation of long work (with DB e.g.)
+        Console.WriteLine("in1 complete");
     });
-    Console.WriteLine("end");
-    
+}
 
+async Task Print2()
+{
+    Console.WriteLine("start2");
+    await Task.Run(() =>
+    {
+        Thread.Sleep(6000); // imitation of long work (with DB e.g.)
+        Console.WriteLine("in2 complete");
+    });
+}
+
+
+async Task Print3()
+{
+    Console.WriteLine("start3");
+    await Task.Run(() =>
+    {
+        Thread.Sleep(3000); // imitation of long work (with DB e.g.)
+        Console.WriteLine("in3 complete");
+    });
 }
